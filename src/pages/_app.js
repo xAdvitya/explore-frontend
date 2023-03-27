6ec60e02e4c91347b978eb1c8f02eb8e07d66325
xAdvitya <advityasharma21@gmail.com>
2023-03-27 15:20:55 +0530

    initial

---
+++ src/pages/_app.js
@@ -0,0 +1,16 @@
+import '@/styles/globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import Head from 'next/head';
+
+config.autoAddCss = false;
+
+export default function App({ Component, pageProps }) {
+  <Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+  </Head>;
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { useEffect, useState } from 'react';
+import rawAxios from 'axios';
+
+export default function Home() {
+  return <>
+  <h1>Explore App</h1>
+  </>;
+}
